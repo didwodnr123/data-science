f115d3980f0181657961c498e7bab083b782fab4
jaeukyang <didwodnr123@gmail.com>
2020-12-09 21:57:16 +0900

    [UPDATE]: python list

---
+++ python-basic/list/split.py
@@ -0,0 +1,10 @@
+han = open('mbox-short.txt')
+
+for line in han:
+    line = line.rstrip()
+    wds = line.split()
+    # guardian
+    if len(wds) < 1 or wds[0] != 'From':
+        continue
+
+    print(wds[2])
