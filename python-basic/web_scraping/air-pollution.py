bc22a3e17c009082a10e38c7cfbd8d384c402aa1
jaeukyang <didwodnr123@gmail.com>
2020-12-13 15:40:43 +0900

    [FOLDERING]

--- python-basic/web_scraping/air-pollution.py
+++ python-basic/web_scraping/air-pollution.py
@@ -1,9 +1,24 @@
 import urllib.request, urllib.parse, urllib.error
-import xml.etree.ElementTree as ET
+import json
 
 url = 'http://apis.data.go.kr/6410000/GOA/GOA001'
-key = 'sx50AIPlpEpq6NbPlgby9QC4MbtIfDTeo1yOoO3RVLHN%2F2bIR7HdifocqieTgUP6Ykv2dKZnoby%2FYUoJiEK9oQ%3D%3D'
+key = '%2BvoFlW8qaQb%2F%2BsFq82OIdJK7tQL9NsF7Vr%2FGK37%2BD0DTEkspRGRNX4arDqFDOOohXQXRGnCwKtJfIL7hJqU6nw%3D%3D'
+key = urllib.parse.unquote(key)
 queryParams = '?' + urllib.parse.urlencode({ urllib.parse.quote_plus('ServiceKey') : key, urllib.parse.quote_plus('type') : 'json', urllib.parse.quote_plus('numOfRows') : '10', urllib.parse.quote_plus('pageNo') : '1' })
 
 response = urllib.request.urlopen(url + queryParams)
-print(response.read().decode())
+data = response.read().decode()
+
+try:
+    js = json.loads(data)
+except:
+    print('urlopen error')
+
+for item in js['items']:
+    if item == { }:
+        print('None')
+        continue
+    if item['area'] == '성남시':
+        print(item)
+
+print('item counts:', len(js['items']))
--- python-basic/web_scraping/covid19.py
+++ python-basic/web_scraping/covid19.py
@@ -2,8 +2,11 @@ import urllib.request, urllib.parse, urllib.error
 import xml.etree.ElementTree as ET
 
 url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson'
-key = 'sx50AIPlpEpq6NbPlgby9QC4MbtIfDTeo1yOoO3RVLHN%2F2bIR7HdifocqieTgUP6Ykv2dKZnoby%2FYUoJiEK9oQ%3D%3D'
-queryParams = '?' + urllib.parse.urlencode({ urllib.parse.quote_plus('ServiceKey') : key, urllib.parse.quote_plus('type') : 'json', urllib.parse.quote_plus('numOfRows') : '10', urllib.parse.quote_plus('pageNo') : '1' })
+key = 'OxcQOljEruD%2B5ytkRbJHdkIzgR8D7%2FTuF5HAu2ulicBKtYI2cGZlSjqoqWJqiI8oM%2Bm2QUcbzQsk99PdsfiiIw%3D%3D'
+key = urllib.parse.unquote(key)
+queryParams = '?' + urllib.parse.urlencode({ urllib.parse.quote_plus('ServiceKey') : key, urllib.parse.quote_plus('pageNo') : '1', urllib.parse.quote_plus('numOfRows') : '10', urllib.parse.quote_plus('startCreateDt') : '20200310', urllib.parse.quote_plus('endCreateDt') : '20200315' })
 
 response = urllib.request.urlopen(url + queryParams)
-print(response.read().decode())
+data = response.read().decode()
+
+print(data)
