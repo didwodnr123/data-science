a992df4f8071ba0d2f877ac534f9ff420739306e
Jaeuk <didwodnr123@gmail.com>
2021-01-02 16:17:09 +0900

    [DELETE] private key

---
+++ machine_learning_Jun_Ji/google vision api/label_detect.py
@@ -0,0 +1,14 @@
+from google.cloud import vision
+
+image_uri = 'gs://cloud-sample-data/vision/using_curl/shanghai.jpeg'
+
+client = vision.ImageAnnotatorClient()
+image = vision.types.Image()
+image.source.image_uri = image_uri
+
+response = client.label_detection(image=image)
+
+print('Labels (and confidence score):')
+print('=' * 79)
+for label in response.label_annotations:
+    print(f'{label.description} ({label.score * 100.:.2f}%)')
