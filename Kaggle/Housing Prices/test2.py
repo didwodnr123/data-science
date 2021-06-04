e75fe51229edc473a2f35647fdd3fdba8a1ab5a1
Jaeuk <didwodnr123@gmail.com>
2021-06-04 13:52:58 +0900

    initial commit

---
+++ Kaggle/Housing Prices/test2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  1 15:57:53 2021
+
+@author: yangjaeug
+"""
+
+import pandas as pd
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+
+print('hello')  
